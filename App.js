61ec428357c93f7585da7741d7ca3a430be7beb8
AshCoxam <ccoxam@amiltone.com>
2021-03-01 09:46:01 +0100

    [FIX]IOS Images

--- App.js
+++ App.js
@@ -41,22 +41,6 @@ const AppNavigator = createStackNavigator(
               { scaleY }
             ]
           };
-
-        // const height = layout.initHeight;
-        // const translateY = position.interpolate({
-        //   inputRange: [index - 1, index, index + 1],
-        //   outputRange: [height, 0, 0],
-        // });
-        //
-        // const opacity = position.interpolate({
-        //   inputRange: [index - 1, index - 0.99, index],
-        //   outputRange: [0, 1, 1],
-        // });
-        //
-        // return {
-        //   opacity,
-        //   transform: [{ translateY }]
-        // };
       },
     }),
     defaultNavigationOptions: {
